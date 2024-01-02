2480350da03f577ffb9ecb33dfc94702bbba7b31
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-01-01 22:56:42 -0300

    Feature: Auto Import

--- app.vue
+++ app.vue
@@ -1,3 +1,18 @@
 <template>
   <div>Hello World</div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
 </template>
+
+<script setup>
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>

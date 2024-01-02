1a832e01a8e617f92a29c5129a5ccd9e5154b73e
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-01-02 11:02:59 -0300

    Feature: Components Directory

--- app.vue
+++ app.vue
@@ -1,18 +1,7 @@
 <template>
   <div>Hello World</div>
-  <button @click="increment">Increment</button>
-  <p>{{ count }}</p>
-  <button @click="decrement">Decrement</button>
+  <Counter />
 </template>
 
 <script setup>
-const count = ref(0);
-
-const increment = () => {
-  count.value++;
-};
-
-const decrement = () => {
-  count.value--;
-};
 </script>
---
+++ components/Counter/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
+</template>
+
+<script setup>
+const count = ref(0);
+
+const increment = () => {
+  if (count.value === 10) {
+    alert("You've reached the maximum number of votes!");
+  } else {
+    count.value++;
+  }
+};
+
+const decrement = () => {
+  if (count.value === 0) {
+    alert("You've reached the minimum number of votes!");
+  } else {
+    count.value--;
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
